cb6ee923d149516fcbb680b01fe058814771a9ff
remiguillette <remiguillette@gmail.com>
2025-04-22 19:30:17 +0000

    fix favbare menu

--- dist/app.js
+++ dist/app.js
@@ -33352,6 +33352,44 @@ var NavBar = function NavBar(_ref) {
     label: 'Bureau',
     icon: '🏠'
   }, {
+    id: 'grid',
+    label: 'Grid',
+    icon: /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "24",
+      height: "24",
+      viewBox: "0 0 24 24",
+      fill: "none",
+      stroke: "currentColor",
+      strokeWidth: "2",
+      strokeLinecap: "round",
+      strokeLinejoin: "round"
+    }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("rect", {
+      width: "7",
+      height: "7",
+      x: "3",
+      y: "3",
+      rx: "1"
+    }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("rect", {
+      width: "7",
+      height: "7",
+      x: "14",
+      y: "3",
+      rx: "1"
+    }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("rect", {
+      width: "7",
+      height: "7",
+      x: "14",
+      y: "14",
+      rx: "1"
+    }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("rect", {
+      width: "7",
+      height: "7",
+      x: "3",
+      y: "14",
+      rx: "1"
+    }))
+  }, {
     id: 'settings',
     label: 'Paramètres',
     icon: '⚙️'
